4ac854c530eb64416af72e0ed278af60cf3244bb
Joeli Pikkarainen <joeli.pikkarainen@outlook.com>
2023-09-16 17:07:18 +0300

    Add script to test that inveronment is installed properly

---
+++ test-env.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 16 15:39:11 2023
+
+@author: joeli
+"""
+import pandas
+import tensorflow
+import sklearn
+import matplotlib
+print("Your set up is ok.")
