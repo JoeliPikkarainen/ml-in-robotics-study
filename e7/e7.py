483690a8d5978700d466a42922e4f4712ff5e457
Joeli Pikkarainen <joeli.pikkarainen@outlook.com>
2023-09-20 14:13:56 +0300

    E7 finish assignemnt, there was no problem, but wrongly understood concept

--- e7/e7.py
+++ e7/e7.py
@@ -33,28 +33,38 @@ def forecast_stock_price(in_file_name, in_split,in_prediction_days):
     
     model.fit(x,y)
     predict_train = model.predict(x)
-    df_train['Ennuste'] = predict_train
+    df_train['Ennuste_test'] = predict_train
     
     x_test = np.array(df_test[['Time','Close']])
+    x_train = np.array(df_train[['Time','Close']])
+
     predict_test = model.predict(x_test)
-    df_test['Ennuste'] = predict_test
-    
-    
-    #plt.scatter(df['Date'].values, df['Close'].values, color='black', s=3)
-    plt.plot((df_train['Date'] + pd.DateOffset(days=in_prediction_days)).values , df_train['Ennuste'].values, color='blue', lw=1)
-    plt.plot((df_test['Date']+ pd.DateOffset(days=in_prediction_days)).values, df_test['Ennuste'].values, color='red',lw=1)
+    predict_train = model.predict(x_train)
+
+    df_test['Ennuste_test'] = predict_test
+    df_train['Ennuste_train'] = predict_train
+
+    plt.scatter(df['Date'].values, df['Close'].values, color='black', s=3)
+    plt.plot((df_train['Date'] + pd.DateOffset(days=in_prediction_days)).values , df_train['Ennuste_test'].values, color='blue', lw=1)
+    plt.plot((df_test['Date']+ pd.DateOffset(days=in_prediction_days)).values, df_test['Ennuste_test'].values, color='red',lw=1)
     plt.rcParams['figure.dpi'] = 1024
 
     plt.show()
     
-    df_validation = df_test.dropna()
-    mean_abs_error = mean_absolute_error(
-                        df_validation['CloseFuture'],
-                        df_validation['Ennuste'])
+    df_validation_test = df_test.dropna()
+    df_validation_train = df_train.dropna()
+
+    mean_abs_error_test = mean_absolute_error(
+                        df_validation_test['CloseFuture'],
+                        df_validation_test['Ennuste_test'])
     
-    print("Prediction mean error is %.f" %
-          mean_abs_error)
+    mean_abs_error_train = mean_absolute_error(
+                        df_validation_train['CloseFuture'],
+                        df_validation_train['Ennuste_train'])
+    
+    print("Prediction mean error is train: %.f test: %.f" %
+          (mean_abs_error_test, mean_abs_error_train))
     
     print("Prediction coefficents \n", model.coef_, model.intercept_)
     
-    return 0
+    return df_test
--- e7/e7_driver.py
+++ e7/e7_driver.py
@@ -7,7 +7,5 @@ Created on Tue Sep 19 11:29:51 2023
 
 from e7 import forecast_stock_price
 
-forecast_0 = forecast_stock_price('Google_Stock_Price.csv', 185, 0)
-forecast_15 = forecast_stock_price('Google_Stock_Price.csv', 185, 15)
+forecast_7 = forecast_stock_price('Google_Stock_Price.csv', 185, 7)
 forecast_60 = forecast_stock_price('Google_Stock_Price.csv', 185, 60)
-forecast_100 = forecast_stock_price('Google_Stock_Price.csv', 185, 100)
